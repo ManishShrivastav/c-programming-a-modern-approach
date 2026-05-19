b60a96ca8b8842b76787b87414490a09edf14fa7
Manish Shrivastav <carttoshoppe@gmail.com>
2026-05-19 09:36:57 -0500

    Add exercise 2: explore the relationship between (-i)/j and -(i/j) with justification

---
+++ chapter_4/exercises/exercise_2.c
@@ -0,0 +1,24 @@
+/*
+If i and j are positive integers, does (-i)/j always have the same value as -(i/j)? Jus-
+tify your answer.
+
+a) (-i) / j
+
+    i       j       ans
+    +       +       -
+    -       -       -
+    +       -       +
+    -       +       +
+
+b) -(i / j)
+
+    i       j       ans
+    +       +       -
+    -       -       -
+    +       -       +
+    -       +       +
+
+
+Result -> algebraically identical
+
+*/
