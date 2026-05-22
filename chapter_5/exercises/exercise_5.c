b1e2f3479aeb5d5233f14a3cc7f7505d41100ad3
Manish Shrivastav <carttoshoppe@gmail.com>
2026-05-22 10:31:42 -0500

    Add exercise 5 to illustrate the legal interpretation of chained relational operators in C

--- chapter_5/exercises/exercise_5.c
+++ chapter_5/exercises/exercise_5.c
@@ -54,3 +54,19 @@
 
         n >= 1 && n <= 10
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    printf("Enter a value for n: ");
+    scanf("%d", &n);
+    
+    if (n >= 1 <= 10)
+    {        
+        printf("n is between 1 and 10\n");
+    }
+    return 0;
+}
