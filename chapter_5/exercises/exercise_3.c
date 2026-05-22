fc5b64a26cdbbf23d13765a0ad27e9241ef65df4
Manish Shrivastav <carttoshoppe@gmail.com>
2026-05-22 09:52:54 -0500

    Add comments to clarify expected outputs in exercise 3

--- chapter_5/exercises/exercise_3.c
+++ chapter_5/exercises/exercise_3.c
@@ -35,8 +35,8 @@ int main(void)
 {
     int i, j, k;
     i = 3, j =4, k =5;
-    printf("%d ", i < j || ++j < k);
-    printf("%d %d %d\n", i, j, k);
+    printf("%d ", i < j || ++j < k); // 1
+    printf("%d %d %d\n", i, j, k); // 3 4 5
 
     i = 7; j = 8; k = 9;
     printf("%d ", i - 7 && j++ < k); // 0
