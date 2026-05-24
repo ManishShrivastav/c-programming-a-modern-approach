e65b07fbeed09e0132e9eab4947c52a1cb0cfac4
Manish Shrivastav <carttoshoppe@gmail.com>
2026-05-24 16:13:52 -0500

    Add exercise 1: Implement a program to display powers of 2 up to 128

---
+++ chapter_6/exercises/exercise_1.c
@@ -0,0 +1,25 @@
+/*
+1.
+What output does the following program fragment produce?
+    i = 1;
+    while (i <= 128) {
+    printf("%d ", i);
+    i *= 2;
+    }
+
+My assessment is 1 2 4 8 16 32 64 128
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 1;
+
+    while (i <= 128)
+    {
+        printf("%d ", i);
+        i *= 2;
+    }
+    
+}
