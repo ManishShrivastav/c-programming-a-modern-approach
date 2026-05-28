1b9d540604e445498494acb90f395bf1e5744d60
Manish Shrivastav <carttoshoppe@gmail.com>
2026-05-28 16:30:05 -0500

    Add programming_project_10.c to count vowels in a sentence

---
+++ chapter_7/programming-projects/programming_project_10.c
@@ -0,0 +1,35 @@
+/*
+10.
+Write a program that counts the number of vowels (a, e, i, o, and u) in a sentence:
+Enter a sentence: And that's the way it is.
+Your sentence contains 6 vowels.
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    char c;
+    int total_vowels = 0;
+
+    printf("Enter a sentence : ");
+    scanf("%d", &c);
+    
+    while ((c = getchar()) != '\n')
+    {
+        switch (toupper(c))
+        {
+        case 'A': case 'E': case 'I': case 'O': case 'U':
+            total_vowels ++;
+            break;
+        
+        default:
+            break;
+        }
+    }    
+    
+    printf("Your sentence contains %d vowels", total_vowels);
+    
+    return 0;
+}
