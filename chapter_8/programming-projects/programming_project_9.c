413d81d5cbd3bcc75d6883fbf5be31b568007c66
Manish Shrivastav <carttoshoppe@gmail.com>
2026-05-29 19:17:13 -0500

    Add programming_project_9.c to implement a program for generating a random walk in a 10x10 array

---
+++ chapter_8/programming-projects/programming_project_9.c
@@ -0,0 +1,113 @@
+/*
+9.
+Write a program that generates a “random walk” across a 10 × 10 array. The array will con-
+tain characters (all '.' initially). The program must randomly “walk” from element to ele-
+ment, always going up, down, left, or right by one element. The elements visited by the
+program will be labeled with the letters A through Z, in the order visited. Here’s an example
+of the desired output:
+A . . . . . . . . .
+B C D . . . . . . .
+. F E . . . . . . .
+H G . . . . . . . .
+I . . . . . . . . .
+J . . . . . . . Z .
+K . . R S T U V Y .
+L M P Q . . . W X .
+. N O . . . . . . .
+. . . . . . . . . .
+Hint: Use the srand and rand functions (see deal.c) to generate random numbers.
+After generating a number, look at its remainder when divided by 4. There are four possible
+values for the remainder—0, 1, 2, and 3—indicating the direction of the next move. Before
+performing a move, check that (a) it won’t go outside the array, and (b) it doesn’t take us to
+an element that already has a letter assigned. If either condition is violated, try moving in
+another direction. If all four directions are blocked, the program must terminate. Here’s an
+example of premature termination:
+A B G H I . . . . .
+. C F . J K . . . .
+. D E . M L . . . .
+. . . . N O . . . .
+. . W X Y P Q . . .
+. . V U T S R . . .
+. . . . . . . . . .
+. . . . . . . . . .
+. . . . . . . . . .
+. . . . . . . . . .
+Y is blocked on all four sides, so there’s no place to put Z.
+*/
+
+#include <stdio.h>
+#include <stdlib.h> /* srand(), rand() */
+#include <time.h> /* time() */
+
+int main(void) {
+
+    char letter = 'A', 
+         grid[10][10] = {0};
+    int i = 0,
+        j = 0,
+        up = 0,   /* directions are essentially bools, written as ints to */
+        down = 0, /* conform with C89 standard.                           */
+        left = 0,
+        right = 0,
+        move = 0;
+
+    srand((unsigned) time(NULL));
+
+    grid[i][j] = letter++;
+
+    while (letter <= 'Z') {
+        up = down = left = right = move = 0;
+
+        if (j + 1 < 10 && grid[i][j + 1] == 0)
+            up = 1;
+        if (j - 1 >= 0 && grid[i][j - 1] == 0)
+            down = 1;
+        if (i + 1 < 10 && grid[i + 1][j] == 0)
+            right = 1;
+        if (i - 1 >= 0 && grid[i - 1][j] == 0)
+            left = 1;
+
+        if (up + down + left + right == 0)
+            break;
+
+        move = rand() % 4;
+        
+
+        /* Intentional fallthrough if direction fails */
+        switch(move) {
+            case 0:
+                if (up) {
+                    grid[i][++j] = letter++;
+                    break;
+                }
+            case 1:
+                if (down) {
+                    grid[i][--j] = letter++;
+                    break;
+                }
+            case 2:
+                if (right) {
+                    grid[++i][j] = letter++;
+                    break;
+                }
+            case 3:
+                if (left) {
+                    grid[--i][j] = letter++;
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            if (grid[i][j] == 0) 
+                grid[i][j] = '.';
+            printf("%c ", grid[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
