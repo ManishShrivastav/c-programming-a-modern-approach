c535e31f34128d8054b4706c7a3b841bb9e5fdde
Manish Shrivastav <carttoshoppe@gmail.com>
2026-06-01 15:25:44 -0500

    Add exercise 8: Provide valid and invalid function prototypes for a double parameter

---
+++ chapter_9/exercises/exercise_8.c
@@ -0,0 +1,9 @@
+/*
+8.
+Which of the following would be valid prototypes for a function that returns nothing and has
+one double parameter?
+(a) void f(double x);       ---> Valid prototype
+(b) void f(double);         ---> Valid prototype
+(c) void f(x);              ---> Invalid prototype (missing parameter type)
+(d) f(double x);            ---> Invalid prototype (missing return type)
+*/
