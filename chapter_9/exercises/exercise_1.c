dca16b943664e31c912e35be89d7390b72debfd7
Manish Shrivastav <carttoshoppe@gmail.com>
2026-06-01 13:43:18 -0500

    Fix errors in triangle_area function and update comments in exercise 1

---
+++ chapter_9/exercises/exercise_1.c
@@ -0,0 +1,18 @@
+/*
+1.
+The following function, which computes the area of a triangle, contains two errors. Locate
+the errors and show how to fix them. (Hint: There are no errors in the formula.)
+    double triangle_area(double base, height)
+    double product;
+    {
+        product = base * height;
+        return product / 2;
+    }
+*/
+
+double triangle_area(double base, double height) // Added 'double' before 'height'
+{
+    double product; // Moved the declaration of 'product' inside the function body
+    product = base * height;
+    return product / 2;
+}
